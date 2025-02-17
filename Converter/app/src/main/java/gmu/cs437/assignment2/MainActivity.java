01744bbe0497489a12537836d881b3f1340b74a1
Ricardo Amaya <ricdamayar@gmail.com>
2025-02-17 18:43:05 -0500

    A3: functionality implementation for accuracy spinner.

--- Converter/app/src/main/java/gmu/cs437/assignment2/MainActivity.java
+++ Converter/app/src/main/java/gmu/cs437/assignment2/MainActivity.java
@@ -20,10 +20,11 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText inputEditText;
     private TextView resultTextView;
-    private Spinner unitSpinner;
+    private Spinner unitSpinner, accuracySpinner;
     private Button clearButton, swapButton;
 
     private List<Conversion> conversions;
+    private int accuracy = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         inputEditText = findViewById(R.id.inputEditText);
         resultTextView = findViewById(R.id.resultTextView);
         unitSpinner = findViewById(R.id.unitSpinner);
+        accuracySpinner = findViewById(R.id.accuracySpinner);
         clearButton = findViewById(R.id.clearButton);
         swapButton = findViewById(R.id.swapButton);
 
@@ -48,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<Conversion> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, conversions);
         unitSpinner.setAdapter(adapter);
 
+        ArrayAdapter<Integer> roundingAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, new Integer[]{0, 1, 2, 3, 4});
+        accuracySpinner.setAdapter(roundingAdapter);
+
         inputEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
@@ -62,6 +67,16 @@ public class MainActivity extends AppCompatActivity {
         unitSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                performConversion();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        accuracySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                accuracy = (int) parent.getItemAtPosition(position);
                 performConversion();
             }
             @Override
@@ -97,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
             Conversion selectedConversion = (Conversion) unitSpinner.getSelectedItem();
             //conversion should always be the correct operation, the swap has already happened
             float result = selectedConversion.conversion.apply(value);
-            resultTextView.setText("Result: " + result);
+            resultTextView.setText("Result: " + String.format("%." + accuracy + "f", result));
         }
     }
 }
