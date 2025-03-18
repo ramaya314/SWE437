51a659571cb5ac16e67fbcd69c1ea6b31e892464
Ricardo Amaya <ricdamayar@gmail.com>
2025-03-17 22:14:44 -0400

    unit tests for user story 2. App does not compile.

--- Converter/app/src/test/java/gmu/cs437/assignment2/TDDTests.java
+++ Converter/app/src/test/java/gmu/cs437/assignment2/TDDTests.java
@@ -3,11 +3,20 @@ package gmu.cs437.assignment2;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 
 public class TDDTests {
+
+
+    private List<Conversion> conversions;
+
+    @Before
+    public void Setup() {
+        conversions = Converter.getProgramConversions();
+    }
 
     @Test
     public void testConvertMphToKph() {
@@ -28,10 +37,37 @@ public class TDDTests {
 
         String leftUnit = "Miles per hour (Mph)";
         String rightUnit = "Kilometers per hour (Kph)";
-        List<Conversion> conversions = Converter.getProgramConversions();
 
         boolean found = conversions.stream().anyMatch(conv ->
-                conv.toString().equals(leftUnit + " -> " + rightUnit) ||
+                conv.toString().equals(leftUnit +  " -> " + rightUnit) ||
+                conv.toString().equals(rightUnit + " -> " + leftUnit)
+        );
+
+        assertTrue(found);
+    }
+
+
+    @Test
+    public void testConvertKelvinToCelsius() {
+        assertEquals(-236.15f, Converter.convertK2C(1.0f), 0.001);
+        assertEquals(-237.15f, Converter.convertK2C(0.0f), 0.001);
+        assertEquals(-238.15, Converter.convertK2C(-1.0f), 0.001);
+    }
+
+    @Test
+    public void testConvertCelsiusToKelvin() {
+        assertEquals(238.15f, Converter.convertC2K(1.0f), 0.001);
+        assertEquals(237.15, Converter.convertC2K(0.0f), 0.001);
+        assertEquals(236.15f, Converter.convertC2K(-1.0f), 0.001);
+    }
+
+    @Test
+    public void testKToCConversionExists() {
+        String leftUnit = "Kelvin (K)";
+        String rightUnit = "Celsius (C)";
+
+        boolean found = conversions.stream().anyMatch(conv ->
+                conv.toString().equals(leftUnit +  " -> " + rightUnit) ||
                 conv.toString().equals(rightUnit + " -> " + leftUnit)
         );
 
